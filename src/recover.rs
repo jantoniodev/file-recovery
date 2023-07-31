ee15a66e5a5c08a5fc464642aa1b9e5822410dcd
jantoniodev <jose.lopezh95@gmail.com>
2023-07-30 23:17:39 -0400

    Aumenta tamaño del buffer

--- src/recover.rs
+++ src/recover.rs
@@ -5,11 +5,11 @@ use std::fs::File;
 
 pub fn recover_png(filepath: &str, output: &str) -> io::Result<i32> {
     let mut file = File::open(filepath)?;
-    let mut buffer = [0; 1];
+    let mut buffer = [0; 32];
 
     let mut png = PNG::new();
     let mut founds = 0;
-    let mut byte: u64 = 0;
+    let mut byte: usize = 0;
 
     loop {
         let n: usize = file.read(&mut buffer)?;
@@ -17,19 +17,21 @@ pub fn recover_png(filepath: &str, output: &str) -> io::Result<i32> {
             break;
         }
 
-        match png.step(buffer[..n][0]) {
-            Some(image) => {
-                founds += 1;
-                let file_name = format!("image{}.png", founds);
-                println!("📁 PNG: {} {:?} bytes", file_name, image.len());
-                save_file(image, output, &file_name);
-            }
-            None => {
-
+        for buffer_byte in buffer.iter_mut() {
+            match png.step(*buffer_byte) {
+                Some(image) => {
+                    founds += 1;
+                    let file_name = format!("image{}.png", founds);
+                    println!("📁 PNG: {} {:?} bytes", file_name, image.len());
+                    save_file(image, output, &file_name);
+                }
+                None => {
+    
+                }
             }
         }
 
-        byte += 1;
+        byte += n;
         print_progress(byte);
     }
 
@@ -42,7 +44,7 @@ fn save_file(file_bytes: Vec<u8>, folder: &str, filename: &str) {
     let _ = file.write_all(&file_bytes);
 }
 
-fn print_progress(progress: u64) {
+fn print_progress(progress: usize) {
     let megabytes = progress / 1000000;
 
     print!("{:?} Mb\r", megabytes);
